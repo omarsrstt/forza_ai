445062738620df1e631ee71158d6575b7a317800
Syed Ibrahim Shakir <omarsrstt@gmail.com>
2024-06-05 17:52:53 +0200

    Capitalize const variables

--- record.py
+++ record.py
@@ -15,10 +15,10 @@ def start_buffer():
         time.sleep(0.5)
 
 # Region of screen capture
-# monitor_region = {"top": 40, "left": -1920, "width": 1730, "height": 960}
-# monitor_region = {"top": 40, "left": -1920, "width": 970, "height": 550}
-# monitor_region = {"top": 0, "left": -1920, "width": 1920, "height": 1080}
-monitor_region = {"top": 0, "left": -1920, "width": 1920, "height": 1080}
+# MONITOR_REGION = {"top": 40, "left": -1920, "width": 1730, "height": 960}
+# MONITOR_REGION = {"top": 40, "left": -1920, "width": 970, "height": 550}
+# MONITOR_REGION = {"top": 0, "left": -1920, "width": 1920, "height": 1080}
+MONITOR_REGION = {"top": 0, "left": -1920, "width": 1920, "height": 1080}
 
 
 def main():
@@ -30,15 +30,15 @@ def main():
     gamepadlistener = GamepadListener()
 
     # Resize input image
-    resize_dims = (224, 224)
+    RESIZE_DIMS = (224, 224)
 
     # Create folder to store recorded data
-    output_dir = "dataset"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    OUTPUT_DIR = "dataset"
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
     
     # Get sequence prefix for storing the data
-    existing_files = os.listdir(output_dir)
+    existing_files = os.listdir(OUTPUT_DIR)
     sequence_numbers = [int(file_name.split('_')[1]) for file_name in existing_files if file_name.startswith('seq') and file_name.split('_')[1].isdigit()]
     max_seq_num = 0 if not sequence_numbers else max(sequence_numbers)
     sequence_number = max(max_seq_num, 0) + 1
@@ -47,23 +47,24 @@ def main():
     # File index for saves
     index = 1
 
+    # Wait some time before starting the recording
     start_buffer()
 
     with mss() as sct:
         while True:
             # Capture the screen
-            image = np.array(sct.grab(monitor_region))
-            resized_image = cv2.resize(image, resize_dims, interpolation=cv2.INTER_AREA)
+            image = np.array(sct.grab(MONITOR_REGION))
+            resized_image = cv2.resize(image, RESIZE_DIMS, interpolation=cv2.INTER_AREA)
 
             # Get controller events
             events = gamepadlistener.get_joystick_events()
 
             # Save images
-            image_path = os.path.join(output_dir, f"{prefix}_image_{sequence_number}_{index}.jpg")
+            image_path = os.path.join(OUTPUT_DIR, f"{prefix}_image_{sequence_number}_{index}.jpg")
             cv2.imwrite(image_path, resized_image)
             
             # Save events
-            events_path = os.path.join(output_dir, f"{prefix}_events_{sequence_number}_{index}.json")
+            events_path = os.path.join(OUTPUT_DIR, f"{prefix}_events_{sequence_number}_{index}.json")
             with open(events_path, 'w') as f:
                 json.dump(events.tolist(), f)
 
